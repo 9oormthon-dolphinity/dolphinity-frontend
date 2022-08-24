01115abf0f003363b1af0748918ce4bee50e81a9
Yangseungchan <uvzone@naver.com>
2022-08-24 15:53:58 +0900

    feature: 카카오 지도 렌더링

---
+++ src/pages/main.tsx
@@ -0,0 +1,10 @@
+import KakaoMap from "components/KakaoMap";
+
+const Main = () => {
+  return (
+    <div>
+      <KakaoMap latitude={33.450701} longitude={126.570667} />
+    </div>
+  );
+};
+export default Main;
