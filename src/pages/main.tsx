35b933be6b8ab17cd1ceb89a08550fb9df81a8fb
Yangseungchan <uvzone@naver.com>
2022-08-25 02:37:10 +0900

    feature: 플로팅 버튼 추가

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,20 @@
-import KakaoMap from "components/KakaoMap";
+import Image from "components/Image";
+import { IconBellPlus, Icon3dCubeSphere, IconAccessible, IconAlertTriangle } from "@tabler/icons";
+import FloatingButton from "components/FloatingButton";
+
+const actions = [
+  { label: "About", icon: <IconBellPlus />, onClick: console.log },
+  { label: "Profile", icon: <Icon3dCubeSphere />, onClick: console.log },
+  { label: "Picture", icon: <IconAccessible />, onClick: console.log },
+  { label: "Trash", icon: <IconAlertTriangle />, onClick: console.log },
+];
 
 const Main = () => {
   return (
-    <div style={{ width: "800px" }}>
-      <KakaoMap latitude={33.450701} longitude={126.570667} />
+    <div style={{ width: "400px", height: "600px", position: "relative" }}>
+      <FloatingButton actions={actions} />
+      {/* <KakaoMap latitude={33.450701} longitude={126.570667} /> */}
+      {/* <Image src="/map.png" alt="카카오 지도" layout="fill" /> */}
     </div>
   );
 };
